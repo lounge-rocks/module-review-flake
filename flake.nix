a43c70893dc6de24c2d08a353ecd13c42289a6d9
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-03-02 14:40:18 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   # inputs.pkgsReview.url = "github:Artturin/nixpkgs/pipewirejackldpath";
-  inputs.pkgsReview.url = "github:stuebinm/nixpkgs/master";
+  # inputs.pkgsReview.url = "github:stuebinm/nixpkgs/master";
+  inputs.pkgsReview.url =
+    "github:stuebinm/nixpkgs?ref=feature/mattermost-secrets";
+
+  # inputs.pkgsReview.url = "/home/pinpox/code/github.com/NixOS/nixpkgs";
   #inputs.pkgsReview.url = "/home/artturin/nixgits/my-nixpkgs";
 
   outputs = inputs@{ self, nixpkgs, pkgsReview }: {
@@ -23,11 +27,69 @@
             "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
           ];
 
+          system.activationScripts.create-mm-secrets = ''
+            cat > /var/lib/mattermost/secrets.json << END
+
+            {
+              "PasswordSettings": {
+                "MinimumLength": 7
+              }
+            }
+
+            END
+          '';
+
           # Test-Specific configuration for the module
           services.mattermost = {
             enable = true;
             mutableConfig = false;
+            siteUrl = "http://localhost";
+
+            secretFile = "/var/lib/mattermost/secrets.json";
+
+            extraConfig = {
+              PasswordSettings = {
+                MinimumLength = 5;
+                Lowercase = false;
+                Number = false;
+                Uppercase = false;
+                Symbol = false;
+              };
+            };
+
           };
+
+          console = {
+            font = "Lat2-Terminus16";
+            keyMap = "colemak";
+          };
+
+          services.qemuGuest.enable = true;
+          networking.firewall.enable = false;
+
+          virtualisation.forwardPorts = [
+
+            # forward local port 2222 -> 22, to ssh into the VM
+            {
+              from = "host";
+              host.port = 2222;
+              guest.port = 22;
+            }
+            {
+              from = "host";
+              host.port = 8065;
+              guest.port = 8065;
+            }
+
+            # forward local port 80 -> 10.0.2.10:80 in the VLAN
+            {
+              from = "guest";
+              guest.address = "10.0.2.10";
+              guest.port = 80;
+              host.address = "127.0.0.1";
+              host.port = 80;
+            }
+          ];
 
           # Documentation for these is in nixos/modules/virtualisation/qemu-vm.nix
           virtualisation = {
@@ -51,6 +113,7 @@
     # 'nix build ".#nixosConfigurations.vm.config.system.build.vm" && ./result/bin/run-nixos-vm'
     defaultPackage.x86_64-linux =
       self.nixosConfigurations.vm.config.system.build.vm;
+
     defaultApp.x86_64-linux = {
       type = "app";
       program = "${self.defaultPackage.x86_64-linux}/bin/run-nixos-vm";
